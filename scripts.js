acc5db0383692f8d8aba32c81e70d49e75be4708
Peter Dahlén <36642622+peterdahlen@users.noreply.github.com>
2023-06-13 11:56:03 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,29 @@
+$('.filter-button').click(function() {
+  var filter = $(this).data('filter');
+      console.log(filter);
+  if (filter === 'all') {
+      $('.filter').show();
+      
+  } else {
+      $('.filter').hide();
+      $('.filter.' + filter).show();
+  }
+
+  $('.filter-button').removeClass('active');
+  $(this).addClass('active');
+});
+
+// Show or hide the button based on scroll position
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 100) {
+  $('#scrollToTopBtn').fadeIn();
+  } else {
+  $('#scrollToTopBtn').fadeOut();
+  }
+});
+
+// Scroll to top when the button is clicked
+$('#scrollToTopBtn').click(function() {
+  $('html, body').animate({ scrollTop: 0 }, 'slow');
+});
+  
